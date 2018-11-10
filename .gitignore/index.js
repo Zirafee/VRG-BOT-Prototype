9ae4644a4841d005ec9e593d14bcf0806352cd79
LePtitFreez <44878524+LePtitFreez@users.noreply.github.com>
2018-11-10 12:45:47 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,19 +4,23 @@ const bot = new Discord.Client();
 var prefix = ("*")
 
 bot.on('ready', function() {
-    bot.user.setActivity("pour de l'aide: *help");
+    bot.user.setActivity("pour de l'aide: *Help");
 });
 
 bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.send("Liste des commandes, n'oubliez pas de mettre: * ,avant la commande comme vous l'avez fait pour faire apparaître cette page: \n -help \n -JDQ");
+    if (message.content === prefix + "Help"){
+        message.channel.send("Liste des commandes, n'oubliez pas de mettre: * ,avant la commande comme vous l'avez fait pour faire apparaître cette page: \n -Help \n -JDQ \n -Creator");
     }
 
     if (message.content === prefix + "JDQ"){
         message.channel.send("Voici ce à quoi je peux répondre: \n -Salut \n -salut \n -yop \n Evidemment ily aura plus de répliques dans le futur et vous pouvez m'aider à en avoir plus en proposant vos idées dans Rue de la remarque !");
+    }
+
+    if (message.content === prefix + "Creator"){
+        message.channel.send("Mon créateur est Freez, le projet est evidement supervisé par Napidou.")
     }
     
     if (message.content === "Salut"){
