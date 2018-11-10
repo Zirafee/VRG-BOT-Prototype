49b4336e55f671232e46fc83968280c37082189d
LePtitFreez <44878524+LePtitFreez@users.noreply.github.com>
2018-11-10 13:18:03 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,18 +11,18 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-    if (message.content === prefix + "Help"){
-        message.channel.send("Liste des commandes, n'oubliez pas de mettre: * ,avant la commande comme vous l'avez fait pour faire apparaître cette page: \n -Help \n -JDQ \n -Creator");
-    }
-
     if (message.content === prefix + "JDQ"){
         message.channel.send("Voici ce à quoi je peux répondre: \n -Salut \n -salut \n -yop \n Evidemment ily aura plus de répliques dans le futur et vous pouvez m'aider à en avoir plus en proposant vos idées dans Rue de la remarque !");
     }
 
     if (message.content === prefix + "Creator"){
-        message.channel.send("Mon créateur est Freez, le projet est evidement supervisé par Napidou.")
+        message.channel.send("Mon créateur est Le Ptit Freez, le projet est supervisé par Napidou.");
     }
-    
+
+    if (message.content === prefix + "Napidou"){
+        message.channel.send("Voici la chaîne de [Napidou](https://www.youtube.com/channel/UCp9Ad5p8AGpPzoXZCCLjeyQ/videos?view_as=subscriber)");
+    }
+
     if (message.content === "Salut"){
         message.reply("Salut moi c'est VRG BOT");
     }
@@ -38,4 +38,18 @@ bot.on('message', message => {
     if (message.content === "bonsoir"){
         message.reply("salut ! Sinon qui bs avec toi ?");
     }
+
+    if (message.content === prefix + "Help"){
+        var embed = new Discord.RichEmbed()
+            .setTitle("Commandes VRG BOT")
+            .setDescription("Voici les commandes que je peux effectué:")
+            .addField("*Help","Affiche cette page d'aide", true)
+            .addField("*JDQ","Affiche les répliques que je peux dire", true)
+            .addField("*Creator","Affiche le pseudo de mon créateur", true)
+            .addField("*Napidou","Envoie le lien de la chaîne de Napidou !", true)
+            .setColor("0xB40431")
+            .setFooter("D'autres fonctionnalités seront instaurées dans le futur")
+        message.channel.sendEmbed(embed);
+    }
+    
 });
