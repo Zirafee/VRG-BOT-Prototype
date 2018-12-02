8c02239120e8b8424f5208f2a2c4f4600596cf35
LePtitFreez <44878524+LePtitFreez@users.noreply.github.com>
2018-12-02 12:14:04 +0100

    [CORRECTION]Oui encore une correction inutile

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("*")
 
 bot.on('ready', function() {
-    bot.user.setActivity("pour de l'aide: *Help");
+    bot.user.setActivity("mise à jour");
 });
 
 bot.login(process.env.TOKEN);
